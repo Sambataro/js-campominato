f69d492dd64bd747d7c39b80c2d525f1582aff4b
fortunato <sambataro1990@gmail.com>
2020-11-19 16:53:18 +0100

    creato i due array

--- js/script.js
+++ js/script.js
@@ -9,14 +9,34 @@
 
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
 
+var arrayBombe = [];
+
+for (var i = 0; i < 16; i++) {
+  arrayBombe.push(numeriRandom(1, 100));
+
+}
+console.log(arrayBombe);
+
 
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+
+var arrayNumeriUtente = [];
+
+for (var i = 0; i < 4; i++) {
+  var mossaUtente = parseInt(prompt("Inserisci un numero da 1 a 100"))
+  arrayNumeriUtente.push(mossaUtente);
+
+}
+
+console.log(arrayNumeriUtente);
+
+
 
 
 
 // funzioni -----------------------------------------
 
-function controlloNumeri(array, number) {
+function controlloArray (array, number) {
   var result = false;
   for (var i = 0; i < array.length; i++) {
     if (number == array[i]) {
